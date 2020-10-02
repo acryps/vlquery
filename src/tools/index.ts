d3c479d83d22ee72fb9e77e17815c7b46c3e19cf
edupes <yo.ey.abc@gmail.com>
2020-10-02 15:59:35 +0200

    add cli version and help

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,10 +1,36 @@
+aimport fs = require("fs");
+
 import { compileQueries } from "./parser";
 import { createContext } from "./context-generator";
 
 const args = process.argv.slice(2);
 
-if (args[0] == "create-context") {
-	createContext();
-} else if (args[0] == "compile") {
-	compileQueries();
+switch (args[0]) {
+	case "create-context":
+		createContext();
+
+		break;
+
+	case "compile":
+		compileQueries();
+
+		break;
+
+	case "version":
+		const config = JSON.parse(fs.readFileSync(`${__dirname}/../../package.json`).toString());
+
+		console.log(`vlquery v${config.verison}`);
+
+		break;
+
+	default: {
+		console.warn(`invalid command: ${args[0]}`);
+		console.group();
+		console.log("create-context: Create database context");
+		console.log("compile: Compile queries in typescript dist");
+		console.log("version: Print vlquery version");
+		console.groupEnd();
+
+		process.exit(1);
+	}
 }
