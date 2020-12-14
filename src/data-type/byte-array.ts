d281d73217d7226f3452e518f3e1d1ae7d5aba61
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2020-12-14 18:52:36 +0100

    fix empty buffer parameter

--- src/data-type/byte-array.ts
+++ src/data-type/byte-array.ts
@@ -2,11 +2,15 @@ import { BaseDataType } from "./base";
 
 export class ByteArray implements BaseDataType {
     static sqlParameterTransform(parameterIndex: number, value: Buffer) {
+        if (!value) {
+            return `$${parameterIndex}`;
+        }
+
         return `VALUES(DECODE($${parameterIndex}::text, 'hex'))`;
     }
 
     static toSQLParameter(value: Buffer) {
-        return value.toString();
+        return value.toString("hex");
     }
 
     static fromSQL(value: string) {
