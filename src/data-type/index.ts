2400487080048ab8ec9fb0ad6b54fef150e5fed3
edupes <yo.ey.abc@gmail.com>
2020-12-27 17:21:28 +0100

    add timestamptz (with timezone) support

--- src/data-type/index.ts
+++ src/data-type/index.ts
@@ -12,8 +12,10 @@ export const dataTypes: { [name: string]: typeof BaseDataType } = {
 	real: BaseDataType,
 	boolean: BaseDataType,
 	uuid: BaseDataType,
-    "timestamp without time zone": BaseDataType,
-    "time without time zone": BaseDataType,
+	"timestamp without time zone": BaseDataType,
+	"timestamp with time zone": BaseDataType,
+	"time without time zone": BaseDataType,
+	"time with time zone": BaseDataType,
     date: BaseDataType,
     json: BaseDataType,
     jsonb: BaseDataType
