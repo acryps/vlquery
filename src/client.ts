4379e104218139d8a03cd0d1834f515a497a6996
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-02-26 18:31:51 +0100

    fix disconnecting request

--- src/client.ts
+++ src/client.ts
@@ -4,16 +4,48 @@ export class DbClient {
 	static connectedClient: DbClient;
 	
 	connection: Client;
+	connected: boolean;
+
+	stalledRequests: StalledDbRequest[] = [];
 
 	constructor(configuration?) {
 		this.connection = new Client(configuration);
+
+		this.connection.on("error", () => this.reconnect());
+		this.connection.on("end", () => this.reconnect());
 	}
 
 	async connect() {
 		await this.connection.connect();
+
+		this.connected = true;
+	}
+
+	async reconnect() {
+		this.connected = false;
+
+		this.connect().then(() => {
+			while (this.stalledRequests.length) {
+				const request = this.stalledRequests.pop();
+
+				this.query(request.query, request.data);
+			}
+		});
 	}
 
 	async query(sql: string, params: any[]) {
+		if (!this.connected) {
+			const stalledRequest = new StalledDbRequest();
+			stalledRequest.query = sql;
+			stalledRequest.data = params;
+
+			this.stalledRequests.push(stalledRequest);
+
+			return new Promise(done => {
+				stalledRequest.oncomplete = data => done(data);
+			});
+		}
+
 		const names = Object.keys(params).sort().reverse();
 		let data = [];
 
@@ -33,4 +65,11 @@ export class DbClient {
 	static async query(sql: string, params: any[]) {
 		return this.connectedClient.query(sql, params);
 	}
+}
+
+export class StalledDbRequest {
+	query: string;
+	data: any[];
+
+	oncomplete(res) {}
 }
