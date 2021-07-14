17b17a36184e034eee346348f3c5045f698dee25
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-07-14 14:05:24 +0200

    add order by null coalesce

--- src/query-operators/order.ts
+++ src/query-operators/order.ts
@@ -7,39 +7,59 @@ import { QueryExtent } from "./extent";
 import { Query } from "../query";
 
 export class QueryOrder<TModel extends Entity<TQueryModel>, TQueryModel extends QueryProxy> {
-	extent: QueryExtent<TModel, TQueryModel>;
-	column: { name: string; type: string; };
+	properties: QueryOrderProperty<TModel, TQueryModel>[];
 
 	constructor(
 		public query: Query<TModel, TQueryModel>,
 		sorter: (item: TQueryModel) => any,
 		public direction: "asc" | "desc"
 	) {
-		const properties = sorter.toString().split("=>")[1].split(".").map(v => v.trim()).slice(1);
+		const body = sorter.toString().split("=>")[1];
+		this.properties = [];
 
-		this.extent = query.rootExtent;
-		let set = query.set as DbSet<Entity<QueryProxy>, QueryProxy>;
+		for (let property of body.split("||")) {
+			const prop = new QueryOrderProperty();
+			const properties = property.trim().split(".").map(v => v.trim()).slice(1);
 
-		for (let i = 0; i < properties.length - 1; i++) {
-			const name = properties[i];
-			const proxy = new set.modelConstructor();
-			const reference = proxy[name] as ForeignReference<Entity<QueryProxy>>;
+			prop.extent = query.rootExtent;
+			let set = query.set as DbSet<Entity<QueryProxy>, QueryProxy>;
+	
+			for (let i = 0; i < properties.length - 1; i++) {
+				const name = properties[i];
+				const proxy = new set.modelConstructor();
+				const reference = proxy[name] as ForeignReference<Entity<QueryProxy>>;
+	
+				const join = new QueryJoin(
+					query,
+					prop.extent,
+					(new reference.$relation()).$$meta.tableName,
+					reference.$$item.$$meta.columns[reference.$column].name
+				);
+	
+				prop.extent = join.extent;
+				set = new reference.$relation().$$meta.set;
+			}
+	
+			prop.column = set.$$meta.columns[properties[properties.length - 1]];
 
-			const join = new QueryJoin(
-				query,
-				this.extent,
-				(new reference.$relation()).$$meta.tableName,
-				reference.$$item.$$meta.columns[reference.$column].name
-			);
-
-			this.extent = join.extent;
-			set = new reference.$relation().$$meta.set;
+			this.properties.push(prop);
 		}
-
-		this.column = set.$$meta.columns[properties[properties.length - 1]];
 	}
 
 	toSQL() {
-		return `${this.extent.name}.${this.column.name} ${this.direction}`;
+		if (this.properties.length == 1) {
+			return `${this.properties[0].toSQL()} ${this.direction}`;
+		}
+
+		return `COALESCE(${this.properties.map(p => p.toSQL()).join(", ")}) ${this.direction}`
+	}
+}
+
+export class QueryOrderProperty<TModel extends Entity<TQueryModel>, TQueryModel extends QueryProxy> {
+	extent: QueryExtent<TModel, TQueryModel>;
+	column: { name: string; type: string; };
+
+	toSQL() {
+		return `${this.extent.name}.${this.column.name}`;
 	}
 }
