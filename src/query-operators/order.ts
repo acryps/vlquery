0864f1d9fcd769dffcd905b234e73cd85635f625
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2021-12-24 18:43:28 +0100

    add fragements to sorter

--- src/query-operators/order.ts
+++ src/query-operators/order.ts
@@ -7,81 +7,34 @@ import { QueryExtent } from "./extent";
 import { Query } from "../query";
 import { CompiledQuery, CompiledQueryCall } from "../compiled-query";
 import { QueryFunction, queryFunctions } from "../functions";
+import { QueryFragment } from "./fragment";
 
 export class QueryOrder<TModel extends Entity<TQueryModel>, TQueryModel extends QueryProxy> {
 	children: QueryOrder<TModel, TQueryModel>[];
 
 	extent: QueryExtent<TModel, TQueryModel>;
-	column: string;
-	calls: CompiledQueryCall[];
+	fragment: QueryFragment<TModel, TQueryModel>;
 
 	constructor(
 		public query: Query<TModel, TQueryModel>,
-		sorterTree: CompiledQuery,
+		tree: CompiledQuery,
 		public direction: "asc" | "desc"
 	) {
-		if (sorterTree.logical) {
+		if (tree.logical) {
 			this.children = [
-				new QueryOrder(query, sorterTree.logical.left, direction),
-				new QueryOrder(query, sorterTree.logical.right, direction)
+				new QueryOrder(query, tree.logical.left, direction),
+				new QueryOrder(query, tree.logical.right, direction)
 			];
 		} else {
-			let path: string[] = [];
-
-			if (sorterTree.call) {
-				this.calls = [];
-
-				for (let item of sorterTree.call.stack) {
-					if (typeof item == "string") {
-						path.push(item);
-					} else {
-						this.calls.push(item);
-					}
-				}
-			} else {
-				path = sorterTree.path;
-			}
-
-			this.extent = query.rootExtent;
-
-			let set = query.set as DbSet<Entity<QueryProxy>, QueryProxy>;
-
-			for (let i = 0; i < path.length - 1; i++) {
-				const name = path[i];
-
-				const proxy = new set.modelConstructor();
-				const reference = proxy[name] as ForeignReference<Entity<QueryProxy>>;
-
-				const join = new QueryJoin(
-					query,
-					this.extent,
-					(new reference.$relation()).$$meta.tableName,
-					reference.$$item.$$meta.columns[reference.$column].name
-				);
-
-				this.extent = join.extent;
-				set = new reference.$relation().$$meta.set;
-			}
-
-			this.column = set.$$meta.columns[path[path.length - 1]].name;
+			this.fragment = new QueryFragment(query, tree);
 		}
 	}
 
 	private toSQLFragment() {
 		if (this.children) {
 			return `COALESCE(${this.children.map(child => child.toSQLFragment()).join(", ")})`;
-		} else if (this.calls) {
-			let body = `(${this.extent.name}.${this.column})`;
-
-			for (let call of this.calls) {
-				const fx = queryFunctions[call.name] as QueryFunction;
-
-				body = fx.toSQL(body, call.parameters);
-			}
-
-			return body;
 		} else {
-			return `${this.extent.name}.${this.column}`;
+			return this.fragment.toSQL();
 		}
 	}
 
